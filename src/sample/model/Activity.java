c2de089d9fae975ff99a02fea8144db22c89dca4
ZabiLarry <31674323+ZabiLarry@users.noreply.github.com>
2019-05-10 12:30:36 +0200

    fixed the abstraction of classes and the constructor for activity and user

--- src/sample/model/Activity.java
+++ src/sample/model/Activity.java
@@ -1,4 +1,11 @@
 package sample.model;
 
-public abstract class Activity  {
+public class Activity {
+    private int id;
+    private String name;
+    private String location;
+    private String contact;
+    private String type;
+    private Boolean indoor;
+    private Boolean outdoor;
 }
