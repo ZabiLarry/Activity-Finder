9bfd8cf5ddc2dd7a9ad2bc9b7611eef185927761
kuraido92 <49900964+kuraido92@users.noreply.github.com>
2019-06-04 20:40:17 +0200

    finishedhelp

--- src/sample/controllers/HomePageController.java
+++ src/sample/controllers/HomePageController.java
@@ -26,6 +26,7 @@ public class HomePageController extends AbstractController implements Initializa
     private Button authenticationButton;
 
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
